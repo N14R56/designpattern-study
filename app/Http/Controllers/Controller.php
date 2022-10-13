105a74388ea9d142be0dc2832c4a622f17cde47d
MATHEUS MURRAY <m.murray@vhsys.com.br>
2022-10-13 20:25:11 -0300

    feat: inicialize a rota /adapt

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function adapt()
+    {
+        return response(
+            'Olá Rufinão, seja bem-vindo ao nosso microserviço incrível!'
+        );
+    }
 }
